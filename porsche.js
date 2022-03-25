e5a6998d6f131435db3a9f9c8e3c4c74d3966adb
Brandon Neves <brandonneves@brandons-mbp.lan>
2022-03-24 21:51:33 -0400

    Initial Commit and Framework setup

---
+++ porsche.js
@@ -0,0 +1,158 @@
+const startButton = document.querySelector("startbtn");
+const button1 = document.querySelector("btn-1")
+const button2 = document.querySelector("btn-2")
+const button3 = document.querySelector("btn-3")
+const button4 = document.querySelector("btn-4")
+
+
+//move to pg 1
+// startButton.addeventListener('click', startGame);
+// function startGame() {
+//   console.log("game started");
+// }
+
+// for (let i = 0; index < questions.length; index++) {
+//   const element = array[index];
+  
+// }
+
+// let questions = [
+//   {
+//     question: "What was the original model name for the Porsche 911?",
+//     options: [
+//       "900",
+//       "901",
+//       "912",
+//       "beetle"
+//     ]
+//     answer: 1
+//   }
+
+//   {
+//     question: "In what year did the compoany make its first vehicle?",
+//     options: [
+//       "1936",
+//       "1955",
+//       "1944",
+//       "1948"
+//     ]
+//     answer: 3
+//   }
+
+//   {
+//     question: "Porsche is famous at the LeMans 24 hour endurance race.  How many victories do they have?",
+//     options: [
+//       "8",
+//       "28",
+//       "4",
+//       "19"
+//     ]
+//     answer: 3
+//   }
+
+//   {
+//     question: "What type of engine does the Porsche 911 have?",
+//     options: [
+//       "V8",
+//       "V12",
+//       "Flat 6",
+//       "4 cylinder"
+//     ]
+//     answer: 2
+//   }
+
+//   {
+//     question: "Porsche is the worlds largest manufacturer of What?",
+//     options: [
+//       "Flat 6 Engines",
+//       "Racecars",
+//       "SUVs",
+//       "Sportscars"
+//     ]
+//     answer: 1
+//   }
+
+//   {
+//     question: "What country is Porsche from?",
+//     options: [
+//       "Germany",
+//       "France",
+//       "Italy",
+//       "USA"
+//     ]
+//     answer: 0
+//   }
+
+//   {
+//     question: "What is the top of the line 911 model name?",
+//     options: [
+//       "Carrera 4S",
+//       "911 Turbo",
+//       "GT2 RS",
+//       "911 Turbo S"
+//     ]
+//     answer: 2
+//   }
+
+//   {
+//     question: "What company owns Porsche?",
+//     options: [
+//       "Volkswagen",
+//       "Ferrari",
+//       "Ford",
+//       "Mercedes"
+//     ]
+//     answer: 0
+//   }
+
+//   {
+//     question: "What animal is on the Porsche Crest?",
+//     options: [
+//       "Eagle",
+//       "Lion",
+//       "Prancing Horse",
+//       "Bat"
+//     ]
+//     answer: 2
+//   }
+
+//   {
+//     question: "What is the name of Porsche's first SUV?",
+//     options: [
+//       "Macan",
+//       "Cayenne",
+//       "Taycan",
+//       "Panamera"
+//     ]
+//     answer: 1
+//   }
+// ]
+
+//Event listener to change button color on click
+button1.addEventListener("click", () => {
+  button1.style.backgroundColor = "yellow";
+  button1.style.color = "white";
+  console.log("clicked");
+})
+
+button2.addEventListener("click", () => {
+  button1.style.backgroundColor = "yellow";
+})
+
+button3.addEventListener("click", () => {
+  button1.style.backgroundColor = "yellow";
+})
+
+button4.addEventListener("click", () => {
+  button1.style.backgroundColor = "yellow";
+})
+
+function nextQuestion() {
+
+
+}
+
+function answerSelect() {
+
+
+}
